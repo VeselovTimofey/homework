19228cc6c5544d4d8bd8c61d528792d7f82d5948
VeselovTimofey <72414414+VeselovTimofey@users.noreply.github.com>
2023-04-27 07:59:59 +0300

    Update last_message.sql

--- last_message.sql
+++ last_message.sql
@@ -1,8 +1,7 @@
 SELECT user.name, message.text
-FROM user, message,
-    (SELECT user.id, user.name, MAX(message.date)
-    FROM user
-    INNER JOIN message
-      ON user.id = message.user_id
-    GROUP BY user.id) as max_date
+FROM user, message, (SELECT user.id, user.name, MAX(message.date)
+                     FROM user
+                     INNER JOIN message
+                       ON user.id = message.user_id
+                     GROUP BY user.id) as max_date
 WHERE user.id = message.user_id AND user.name = max_date.name AND message.date = max_date.max;
