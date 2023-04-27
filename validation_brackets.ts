f57d322f848473c008ac13ea8b0489e4c801c8b0
VeselovTimofey <72414414+VeselovTimofey@users.noreply.github.com>
2023-04-27 15:36:28 +0300

    update validation_brackets.ts

--- validation_brackets.ts
+++ validation_brackets.ts
@@ -6,19 +6,31 @@ function validation_string(string_of_brackets: string): boolean
         return false;
     }
     let answer: boolean = true;
-    var element_counter = {'(': 0, '[': 0, '{': 0}
-    var double = {')': '(', ']': '[', '}': '{'};
-    let element : any;
+    type Element_Counter =
+    {
+        '(': number,
+        '[': number,
+        '{': number
+    };
+    type Double =
+    {
+        ')': string,
+        ']': string,
+        '}': string
+    };
+    var element_counter: Element_Counter = {'(': 0, '[': 0, '{': 0}
+    var double: Double = {')': '(', ']': '[', '}': '{'};
+    let element: string;
     for (element of string_of_brackets)
     {
         if (element in element_counter)
         {
-            element_counter[element] = element_counter[element] + 1;
+            element_counter[element as keyof Element_Counter] = element_counter[element as keyof Element_Counter] + 1;
         }
-        else if (element in double && element_counter[double[element]] > 0)
+        else if (element in double && element_counter[double[element as keyof Double] as keyof Element_Counter] > 0)
         // checking that the element is a right bracket and has a pair
         {
-            element_counter[double[element]] = element_counter[double[element]] - 1;
+            element_counter[double[element as keyof Double] as keyof Element_Counter] = element_counter[double[element as keyof Double] as keyof Element_Counter] - 1;
         }
         else
          // if there is something other than bracket, exit the loop
@@ -31,7 +43,7 @@ function validation_string(string_of_brackets: string): boolean
     for (count in element_counter)
      // checking that we don't have brackets without a pair
      {
-        if (element_counter[count] != 0)
+        if (element_counter[count as keyof Element_Counter] != 0)
         {
             answer = false;
             break;
